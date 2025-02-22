eabdefa43fbd460728aa17e749406f47b3beaa50
Ashwin Pawar <ashwinpawar62@gmail.com>
2025-02-22 21:36:00 +0530

    Mandatory Field Changes

--- src/main/java/com/demo/swagger/dto/UserDTO.java
+++ src/main/java/com/demo/swagger/dto/UserDTO.java
@@ -10,37 +10,38 @@ import jakarta.validation.constraints.Pattern;
 
 public class UserDTO {
     
-    @NotNull(message = "Email is mandatory")
-    @NotBlank(message = "Email cannot be blank")
-    @Email(message = "Please provide a valid email address")
-    @Schema(example = "user@example.com", required = true, description = "Email address of the user")
-    private String email;
-    
-    @NotNull(message = "Password is mandatory")
-    @NotBlank(message = "Password cannot be blank")
-    @Schema(example = "yourpassword", required = true, description = "Password for the user account")
-    private String password;
-    
-    @NotNull(message = "Role is mandatory")
-    @NotBlank(message = "Role cannot be blank")
-    @Pattern(regexp = "^(Creator|Promoter)$", message = "Role must be either 'Creator' or 'Promoter'")
-    @Schema(
-        example = "Creator", 
-        required = true, 
-        allowableValues = {"Creator", "Promoter"},
-        description = "User role - must be either Creator or Promoter"
-    )
-    private String role;
-    
-    @NotNull(message = "Phone number is mandatory")
-    @NotBlank(message = "Phone number cannot be blank")
-    @Pattern(regexp = "^\\+[1-9]\\d{1,14}$", message = "Phone number must be in international format (e.g., +11234567890)")
-    @Schema(
-        example = "+11234567890", 
-        required = true,
-        description = "Phone number in international format with country code"
-    )
-    private String phoneNumber;
+	// Update in UserDTO.java
+	@NotNull(message = "Email is mandatory")
+	@NotBlank(message = "Email cannot be blank")
+	@Email(message = "Please provide a valid email address")
+	@Schema(example = "ashwin10@gmail.com", required = true, description = "Email address of the user")
+	private String email;
+
+	@NotNull(message = "Password is mandatory")
+	@NotBlank(message = "Password cannot be blank")
+	@Schema(example = "yourpassword", required = true, description = "Password for the user account")
+	private String password;
+
+	@NotNull(message = "Role is mandatory")
+	@NotBlank(message = "Role cannot be blank")
+	@Pattern(regexp = "^(Creator|Promoter)$", message = "Role must be either 'Creator' or 'Promoter'")
+	@Schema(
+	    example = "Creator", 
+	    required = true, 
+	    allowableValues = {"Creator", "Promoter"},
+	    description = "User role - must be either Creator or Promoter"
+	)
+	private String role;
+
+	@NotNull(message = "Phone number is mandatory")
+	@NotBlank(message = "Phone number cannot be blank")
+	@Pattern(regexp = "^\\+[1-9]\\d{1,14}$", message = "Phone number must be in international format (e.g., +11234567890)")
+	@Schema(
+	    example = "+11234567890", 
+	    required = true,
+	    description = "Phone number in international format with country code"
+	)
+	private String phoneNumber;
     
     // Status is optional in DTO as it defaults to ACTIVE
     @Schema(
